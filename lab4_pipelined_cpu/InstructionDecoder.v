3e88b33e42064eee7bf86183f143725fbc594bb2
chseong <chseong02@gmail.com>
2024-04-17 19:04:18 +0900

    CHG: decoder port

--- lab4_pipelined_cpu/InstructionDecoder.v
+++ lab4_pipelined_cpu/InstructionDecoder.v
@@ -6,6 +6,8 @@ module InstructionDecoder (
     rs1,               // output
     rs2,               // output
     rd,                // output
+    is_sub,            // output
+    funct3,            // output
     full_instruction   // output
 );
 
@@ -14,6 +16,8 @@ module InstructionDecoder (
     output reg [4:0] rs1;
     output reg [4:0] rs2;
     output reg [4:0] rd;
+    output reg is_sub;
+    output reg [2:0] funct3;
     output reg [`kInstructionWidth-1:0] full_instruction;
 
     always @(*)begin
@@ -21,6 +25,8 @@ module InstructionDecoder (
         rs1 = instruction[19:15];
         rs2 = instruction[24:20];
         rd = instruction[11:7];
+        is_sub = instruction[30];
+        funct3 = instruction[14:12];
         full_instruction = instruction;
     end
 endmodule
