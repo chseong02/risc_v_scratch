15ec3a7c2facc29e17d6109ee429bf53d27c8876
chseong <chseong02@gmail.com>
2024-04-17 19:10:00 +0900

    CHG: control unit alu src add

--- lab4_pipelined_cpu/ControlUnit.v
+++ lab4_pipelined_cpu/ControlUnit.v
@@ -5,6 +5,7 @@ module ControlUnit (
     mem_read,      // output
     mem_to_reg,    // output
     mem_write,     // output
+    alu_src,       // output
     write_enable,  // output
     is_ecall       // output
 );
@@ -12,6 +13,7 @@ module ControlUnit (
     output reg mem_read;
     output reg mem_to_reg;
     output reg mem_write;
+    output reg alu_src;
     output reg write_enable;
     output reg is_ecall;
 
@@ -19,6 +21,7 @@ module ControlUnit (
         mem_read = part_of_inst == `LOAD;
         mem_to_reg = part_of_inst == `LOAD;
         mem_write = part_of_inst == `STORE;
+        alu_src = part_of_inst != `ARITHMETIC && part_of_inst != `BRANCH;
         write_enable = part_of_inst != `STORE && part_of_inst != `BRANCH && part_of_inst != `ECALL;
         pc_to_reg = part_of_inst == `JAL || part_of_inst == `JALR;
         is_ecall = part_of_inst == `ECALL;
