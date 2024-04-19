7c8ba1ecd30ba8d9f680e3d793cdecf803776d9b
chseong <chseong02@gmail.com>
2024-04-19 20:59:27 +0900

    CHG: mux2_1 parameterized

--- lab4_pipelined_cpu/mux2_1.v
+++ lab4_pipelined_cpu/mux2_1.v
@@ -1,16 +1,16 @@
 `include "constants.v"
 
-module Mux2_1 (
+module Mux2_1 #(parameter data_width = `kDataWidth) (
     in_0, // input
     in_1, // input
     cond, // input
     out   // output
 );
 
-    input [`kDataWidth-1:0] in_0;
-    input [`kDataWidth-1:0] in_1;
+    input [data_width-1:0] in_0;
+    input [data_width-1:0] in_1;
     input cond;
-    output reg [`kDataWidth-1:0] out;
+    output reg [data_width-1:0] out;
 
     always @(*)begin
         if(cond) begin
