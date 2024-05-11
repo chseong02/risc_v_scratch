76b8da1df53f5c1c02dbdc7af4a18f6201a6c941
dalmutimelange <dkim011006@postech.ac.kr>
2024-05-12 01:14:47 +0900

    FIX: is_jump_branch

--- lab4_pipelined_cpu_2/BranchPredictionCounter.v
+++ lab4_pipelined_cpu_2/BranchPredictionCounter.v
@@ -1,27 +1,30 @@
 module BranchPredictionCounter (
     input branch_taken,
     input [2:0] input_counter,
+    input is_jump_branch, // is_jal || is_jalr || is_branch
     output reg [2:0] output_counter
 );
 
     always @(posedge clk) begin
-        case(input_counter)
-            2'b11: begin
-                if(branch_taken) input_counter <= 2'b11;
-                else input_counter <=  2'b10;
-            end
-            2'b10: begin
-                if(branch_taken) input_counter <= 2'b11;
-                else input_counter <=  2'b01;
-            end
-            2'b01: begin
-                if(branch_taken) input_counter <= 2'b10;
-                else input_counter <=  2'b00;
-            end
-            2'b00: begin
-                if(branch_taken) input_counter <= 2'b01;
-                else input_counter <=  2'b00;
-            end
-        endcase
+        if(is_jump_branch){
+            case(input_counter)
+                2'b11: begin
+                    if(branch_taken) input_counter <= 2'b11;
+                    else input_counter <=  2'b10;
+                end
+                2'b10: begin
+                    if(branch_taken) input_counter <= 2'b11;
+                    else input_counter <=  2'b01;
+                end
+                2'b01: begin
+                    if(branch_taken) input_counter <= 2'b10;
+                    else input_counter <=  2'b00;
+                end
+                2'b00: begin
+                    if(branch_taken) input_counter <= 2'b01;
+                    else input_counter <=  2'b00;
+                end
+            endcase
+        }
     end
 endmodule
