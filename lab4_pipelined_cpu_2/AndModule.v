f2858ba7718873a0ced833706f6f7408f2fbbc17
chseong <chseong02@gmail.com>
2024-05-12 13:14:06 +0900

    ADD: and module

---
+++ lab4_pipelined_cpu_2/AndModule.v
@@ -0,0 +1,13 @@
+module AndModule (
+    in_1, // input
+    in_2, // input
+    out   // output
+);
+
+    input in_1;
+    input in_2;
+    output out;
+
+    assign out = in_1 && in_2;
+    
+endmodule
