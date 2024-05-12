59304f69c2c0bd9ba6e2b0ac7fa05c43f5837694
dalmutimelange <dkim011006@postech.ac.kr>
2024-05-12 15:33:25 +0900

    FIX: fix BTB table

--- lab4_pipelined_cpu_2/BranchHistotyTable.v
+++ lab4_pipelined_cpu_2/BranchHistotyTable.v
@@ -2,33 +2,28 @@ module BranchHistoryTable (
     input reset, 
     input clk, 
     input is_jump_branch, // is_jal || is_jalr || is_branch
-    input [31:0]actual_PC, 
-    input [4:0]index, 
-    input [24:0]tag,
-    input [1:0]update_counter, // 업데이트할 카운터
-    output reg is_taken,
-    output reg [31:0] predict_PC
+    input [31:0]old_PC, 
+    input [31:0]cal_PC, 
+    input [31:0]current_PC, 
+    input [1:0] update_counter,
+    output reg [31:0] predict_PC,
+    output reg [1:0] counter,
+    output reg pc_mux_control
 );
 
     reg [59:0]BTB[0:31]; // counter = (59:58) / valid bit = (57) / tag = (56,32) / pc (31,0)
     integer i;
 
     always @(*) begin
-            if((tag == BTB[index][56:32]) && (BTB[index][59:58] == 2'b10 || BTB[index][59:58] == 2'b11)&&(BTB[index][57] == 1)) begin
-                predict_PC = BTB[index][31:0];
-                is_taken = 1;
+            if((current_PC[31:7] == BTB[current_PC[6:2]][56:32]) && (BTB[current_PC[6:2]][59:58] == 2'b10 || BTB[current_PC[6:2]][59:58] == 2'b11)&&(BTB[current_PC[6:2]][57] == 1)) begin
+                predict_PC = BTB[current_PC[6:2]][31:0];
+                pc_mux_control = 1;
             end
             else begin
-                predict_PC = PC + 4;
-                is_taken = 0;
+                pc_mux_control = 0;
             end
-            
-            if(is_jump_branch)begin
-                BTB[index][59:58] = update_counter;
-                BTB[index][57] = 1; // valid bit
-                BTB[index][56:32] = tag;
-                BTB[index][31:0] = actual_PC;
-            end
+
+            counter = BTB[old_PC[6:2]][59:58];
     end
 
 
@@ -37,6 +32,12 @@ module BranchHistoryTable (
             counter <= 0;
             for(i = 0 ; i<32 ;i = i+1)
                 BTB[i][57:0] <= 0;
+        end
+        else begin
+            if(is_jump_branch)begin
+                BTB[old_PC[6:2]][59:58] <= update_counter;
+                BTB[old_PC[6:2]][31:0] <= old_PC;
+            end
         end
     end
 endmodule
