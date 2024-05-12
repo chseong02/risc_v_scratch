22dd76da05d9ac5ea75b81aca87e276b7cbcbc64
chseong <chseong02@gmail.com>
2024-05-12 15:38:00 +0900

    FIX: branch predictor assign

--- lab4_pipelined_cpu_2/BranchPredictionCounter.v
+++ lab4_pipelined_cpu_2/BranchPredictionCounter.v
@@ -9,20 +9,20 @@ module BranchPredictionCounter (
         if(is_jump_branch){
             case(input_counter)
                 2'b11: begin
-                    if(branch_taken) input_counter <= 2'b11;
-                    else input_counter <=  2'b10;
+                    if(branch_taken) input_counter = 2'b11;
+                    else input_counter =  2'b10;
                 end
                 2'b10: begin
-                    if(branch_taken) input_counter <= 2'b11;
-                    else input_counter <=  2'b01;
+                    if(branch_taken) input_counter = 2'b11;
+                    else input_counter =  2'b01;
                 end
                 2'b01: begin
-                    if(branch_taken) input_counter <= 2'b10;
-                    else input_counter <=  2'b00;
+                    if(branch_taken) input_counter = 2'b10;
+                    else input_counter =  2'b00;
                 end
                 2'b00: begin
-                    if(branch_taken) input_counter <= 2'b01;
-                    else input_counter <=  2'b00;
+                    if(branch_taken) input_counter = 2'b01;
+                    else input_counter =  2'b00;
                 end
             endcase
         }
